764447036b83f61af34886e0d96a417e916996e9
Hennan Gadelha <hennanhennan@gmail.com>
2020-05-11 00:16:15 -0300

    trabalhando com thread como objeto, utilizando a interface Runnable

--- src/application/AulaThred.java
+++ src/application/AulaThred.java
@@ -8,55 +8,15 @@ public class AulaThred {
 		// TODO Auto-generated method stub
 
 		// processamento pararelo 1
-		new Thread() {
+		Thread rotina1 = new Thread(thread1);
+		rotina1.start();
 
-			public void run() {
+		// -------------------divisao threads--------------------------
 
-				/* codigo da rotina pra executar em paralelo */
-				for (int i = 0; i < 10; i++) {
+		// processamento parelelo 2
 
-					System.out.println("Executando rotina");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-				// fim do codigo pararelo
-
-			}
-
-		}.start();
-		
-		//-------------------divisao threads--------------------------
-		
-		//processamento parelelo 2
-		new Thread() {
-
-			public void run() {
-
-				/* codigo da rotina pra executar em paralelo */
-				for (int i = 0; i < 10; i++) {
-
-					System.out.println("Executando rotina 2");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-				// fim do codigo pararelo
-
-			}
-
-		}.start();
-		
-		
-		
+		Thread rotina2 = new Thread(thread2);
+		rotina2.start();
 
 		System.out.println("Chegou ao fim teste thread");
 
@@ -64,5 +24,48 @@ public class AulaThred {
 		JOptionPane.showMessageDialog(null, "Sistema continua executando p usuario");
 
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+
+			/* codigo da rotina pra executar em paralelo */
+			for (int i = 0; i < 10; i++) {
+
+				System.out.println("Executando rotina 1");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			// fim do codigo pararelo
+
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for (int i = 0; i < 10; i++) {
+
+				System.out.println("Executando rotina 2");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
 
 }
